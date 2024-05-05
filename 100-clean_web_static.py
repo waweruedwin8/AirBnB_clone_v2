251328129c202d907f8382115252f1e64d09f426
waweruedwin8 <waweruedwin8@gmail.com>
2024-05-05 19:32:13 +0300

    imported os module for file 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,7 +3,7 @@
 Fabric script based on the file 2-do_deploy_web_static.py that creates and
 distributes an archive to the web servers
 """ 
-
+import os
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
